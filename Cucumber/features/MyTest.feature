94373c278fe32bee53422c897093a42d58a38e48
shailesh.jadhav <shailesh.jadhav@RPSPLDP1-60.realitypremedia.in>
2019-02-08 12:54:15 +0530

    mypushs

---
+++ Cucumber/features/MyTest.feature
@@ -0,0 +1,6 @@
+@UATTesting
+Feature: MPT 
+  Scenario: Title of your scenario
+    Given Open the Firefox and launch the application
+    When Enter the Username and Password
+    And Reset the credential
---
+++ Cucumber/src/main/java/StepDefinition/Steps.java
@@ -0,0 +1,37 @@
+package StepDefinition;
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;		
+
+
+public class Steps {
+	
+	
+	
+	
+ WebDriver driver;			
+	    		
+	    @Given("^Open the Firefox and launch the application$")					
+	    public void open_the_Firefox_and_launch_the_application() throws Throwable							
+	    {		
+	       System.out.println("********************start");	
+	    }		
+
+	    @When("^Enter the Username and Password$")					
+	    public void enter_the_Username_and_Password() throws Throwable 							
+	    {		
+	      System.out.println("*********started");				
+	    }		
+
+	    @Then("^Reset the credential$")					
+	    public void Reset_the_credential() throws Throwable 							
+	    {		
+	      System.out.println("***********completed*****************");					
+	    }		
+	}
+
+
